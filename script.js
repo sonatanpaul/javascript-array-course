c70b4d067b6dd58e5d635a393cde38d7d5e606bf
sonatanpaul <iamsonatanpaul@gmail.com>
2025-01-12 23:11:11 +0600

    arrray first video practice task is done

--- script.js
+++ script.js
@@ -33,7 +33,7 @@ const friends = ["Bikash", "Anik", "Rayhan"];
 friends.push("Ashraful");
 console.log(friends);
 // Output : (4) ['Bikash', 'Anik', 'Rayhan', 'Ashraful']
-// Note : push() method return array total index and push() method is muteable method
+// Note : push() method return array total index number and push() method is muteable method
 
 // add first element of array
 
@@ -41,4 +41,27 @@ const subject = ["Bangla", "English", "Math"];
 subject.unshift("ICT");
 console.log(subject);
 // Output :  ['ICT', 'Bangla', 'English', 'Math']
-// note : Note : unshift() method return array total index and unshift() method is muteable method
+// note : Note : unshift() method return array total index number and unshift() method is muteable method
+
+// How to Remove Elements from an Array
+// remove last element
+const bestCity = ["Pabna", "Dhaka", "Rajshahi", "Barisal"];
+bestCity.pop();
+console.log(bestCity);
+// Output : (3) ['Pabna', 'Dhaka', 'Rajshahi']
+// Note :  pop() method alaways return actual remove value and pop method is muteable method
+
+//remove first element
+
+const lang = ["Urdu", "Bangla", "English", "Hindi"];
+lang.shift();
+console.log(lang);
+// Output : (3) ['Bangla', 'English', 'Hindi']
+// Note : shift () method alaways return actual remove value and shift method is muteable method
+
+// How to Determine if a Value is an Array
+
+const array = ["Sonatan", 25, "Student"];
+const actualArray = Array.isArray(array);
+console.log(actualArray);
+// Output : true
