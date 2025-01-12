90abf643976df6e91d8c32fb91926b4b87cefaaf
sonatanpaul <iamsonatanpaul@gmail.com>
2025-01-12 22:58:55 +0600

    learn javascript array element add method

--- script.js
+++ script.js
@@ -19,3 +19,26 @@ const number = [1, 2, 3, 4, 5, 6, 8];
 const numberLength = number.length;
 console.log(numberLength);
 // Output : 7
+
+// How to Get Elements from an Array
+
+const names = ["Sonatan", "Anik", "Ashraful", "Akash"];
+names[0]; // Output : Sonatan
+names[2]; // Output: Ashraful
+
+// How to Add Elements to an Array
+
+// add last element of array
+const friends = ["Bikash", "Anik", "Rayhan"];
+friends.push("Ashraful");
+console.log(friends);
+// Output : (4) ['Bikash', 'Anik', 'Rayhan', 'Ashraful']
+// Note : push() method return array total index and push() method is muteable method
+
+// add first element of array
+
+const subject = ["Bangla", "English", "Math"];
+subject.unshift("ICT");
+console.log(subject);
+// Output :  ['ICT', 'Bangla', 'English', 'Math']
+// note : Note : unshift() method return array total index and unshift() method is muteable method
