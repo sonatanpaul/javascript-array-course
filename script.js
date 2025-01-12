023c011765b9685bbf25447a2c2cc4fe72f5b292
sonatanpaul <iamsonatanpaul@gmail.com>
2025-01-12 22:36:25 +0600

    how to create javascript array learn and how to check array length

--- script.js
+++ script.js
@@ -1,1 +1,21 @@
-console.log("Hello World");
+// How to Create an Array in JavaScript?
+// 01.
+const person = ["Shakib Khan", "Amir Khan", "Salman Khan", "Ranbir Kapoor"];
+console.log(person);
+// Output : ['Shakib Khan', 'Amir Khan', 'Salman Khan', 'Ranbir Kapoor']
+
+// 02.
+const newPerson = new Array(1, 2, 3, 4, 5, 6);
+console.log(newPerson);
+// Output: (6) [1, 2, 3, 4, 5, 6]
+
+// 03. Empty Array Element
+const emptyArray = new Array(10);
+console.log(emptyArray);
+// Output : (10) [empty × 10]
+
+/// How to check array length
+const number = [1, 2, 3, 4, 5, 6, 8];
+const numberLength = number.length;
+console.log(numberLength);
+// Output : 7
